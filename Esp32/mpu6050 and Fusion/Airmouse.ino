8162d8407e7a96c00c522b666dcc2b13ef4f27ca
Semen Hzhesiak <Semen_Hzhesiak@epam.com>
2022-06-06 17:43:03 -0400

    added bluetoth and simple mouse move logic

--- Esp32/mpu6050 and Fusion/Airmouse.ino
+++ Esp32/mpu6050 and Fusion/Airmouse.ino
@@ -5,124 +5,98 @@
 #include <Wire.h>
 #include <Adafruit_Sensor_Calibration.h>
 #include <Adafruit_AHRS.h>
+#include <math.h>
+#include <BleMouse.h>
+const float DEG_MULTIPLIER = 14;
+const float SENSITIVITY_THRESHOLD = 0.2;
+BleMouse bleMouse;
 
 Adafruit_MPU6050 mpu;
 Adafruit_Madgwick filter;
 uint32_t timestamp;
 Adafruit_Sensor_Calibration_EEPROM cal;
+float previousXValue;
+float previousYValue;
+
 
 void setup(void) {
   Serial.begin(115200);
   setup_mpu_6050(mpu);
-  // // Try to initialize!
-  // if (!mpu.begin()) {
-  //   Serial.println("Failed to find MPU6050 chip");
-  //   while (1) {
-  //     delay(10);
-  //   }
-  // }
-  // Serial.println("MPU6050 Found!");
-
-  // mpu.setAccelerometerRange(MPU6050_RANGE_4_G);
-  // Serial.print("Accelerometer range set to: ");
-  // switch (mpu.getAccelerometerRange()) {
-  // case MPU6050_RANGE_2_G:
-  //   Serial.println("+-2G");
-  //   break;
-  // case MPU6050_RANGE_4_G:
-  //   Serial.println("+-4G");
-  //   break;
-  // case MPU6050_RANGE_8_G:
-  //   Serial.println("+-8G");
-  //   break;
-  // case MPU6050_RANGE_16_G:
-  //   Serial.println("+-16G");
-  //   break;
-  // }
-  // mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  // Serial.print("Gyro range set to: ");
-  // switch (mpu.getGyroRange()) {
-  // case MPU6050_RANGE_250_DEG:
-  //   Serial.println("+- 250 deg/s");
-  //   break;
-  // case MPU6050_RANGE_500_DEG:
-  //   Serial.println("+- 500 deg/s");
-  //   break;
-  // case MPU6050_RANGE_1000_DEG:
-  //   Serial.println("+- 1000 deg/s");
-  //   break;
-  // case MPU6050_RANGE_2000_DEG:
-  //   Serial.println("+- 2000 deg/s");
-  //   break;
-  // }
-
-  // mpu.setFilterBandwidth(MPU6050_BAND_94_HZ);
-  // Serial.print("Filter bandwidth set to: ");
-  // switch (mpu.getFilterBandwidth()) {
-  // case MPU6050_BAND_260_HZ:
-  //   Serial.println("260 Hz");
-  //   break;
-  // case MPU6050_BAND_184_HZ:
-  //   Serial.println("184 Hz");
-  //   break;
-  // case MPU6050_BAND_94_HZ:
-  //   Serial.println("94 Hz");
-  //   break;
-  // case MPU6050_BAND_44_HZ:
-  //   Serial.println("44 Hz");
-  //   break;
-  // case MPU6050_BAND_21_HZ:
-  //   Serial.println("21 Hz");
-  //   break;
-  // case MPU6050_BAND_10_HZ:
-  //   Serial.println("10 Hz");
-  //   break;
-  // case MPU6050_BAND_5_HZ:
-  //   Serial.println("5 Hz");
-  //   break;
-  // }
 
   filter.begin(66);
   cal.begin();
   cal.loadCalibration();
-  Serial.println("");
+  bleMouse.begin();
   Wire.setClock(400000); 
   delay(100);
 }
 
 void loop() {
-  if ((millis() - timestamp) < (15)) {
-    return;
+  if(bleMouse.isConnected()) {
+    if ((millis() - timestamp) < (15)) {
+      return;
+    }
+    timestamp = millis();
+
+    /* Get new sensor events with the readings */
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+    cal.calibrate(a);
+    cal.calibrate(g);
+
+    float gx = g.gyro.x * SENSORS_RADS_TO_DPS;
+    float gy = g.gyro.y * SENSORS_RADS_TO_DPS;
+    float gz = g.gyro.z * SENSORS_RADS_TO_DPS;
+
+    filter.updateIMU(gx, gy, gz,
+              a.acceleration.x, a.acceleration.y, a.acceleration.z);
+
+    float yaw = filter.getYaw();
+    float pitch = filter.getPitch();
+    float roll = filter.getRoll();
+
+    float beta = filter.getBeta();
+
+    float xMoveValue = previousXValue - yaw;
+    if (previousXValue != 0 && abs(xMoveValue) <= SENSITIVITY_THRESHOLD) {
+      xMoveValue = 0;
+    }
+
+    float yMoveValue = previousYValue - roll;
+    if (previousYValue != 0 && abs(yMoveValue) <= SENSITIVITY_THRESHOLD) {
+      yMoveValue = 0;
+    }
+
+    previousXValue = yaw;
+    previousYValue = roll;
+
+    // Serial.print("Orientation: ");
+    // Serial.print(previousXValue);
+    // Serial.print(", ");
+    // Serial.print(yaw);
+    // Serial.print(", ");
+    // Serial.print(previousYValue);
+    // Serial.print(", ");
+    // Serial.print(roll);
+
+
+    // if (yMoveValue != 0 || xMoveValue !=0) {
+    //   // Move shit
+    //   Serial.print("Mouse move: ");
+    //   Serial.print(xMoveValue);
+    //   Serial.print(", ");
+    //   Serial.println(yMoveValue);
+
+    //   bleMouse.move(round(xMoveValue * DEG_MULTIPLIER), -round(yMoveValue * DEG_MULTIPLIER));
+    // }
+    
+    Serial.print("Orientation: ");
+    Serial.print(pitch);
+    Serial.print(", ");
+    Serial.print(yaw);
+    Serial.print(", ");
+    Serial.print(roll);
+    Serial.print(", ");
+    Serial.println(beta);
   }
-  timestamp = millis();
-
-  /* Get new sensor events with the readings */
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
-  cal.calibrate(a);
-  cal.calibrate(g);
-
-  float gx = g.gyro.x * SENSORS_RADS_TO_DPS;
-  float gy = g.gyro.y * SENSORS_RADS_TO_DPS;
-  float gz = g.gyro.z * SENSORS_RADS_TO_DPS;
-
-  filter.updateIMU(gx, gy, gz,
-            a.acceleration.x, a.acceleration.y, a.acceleration.z);
-
-  float yaw = filter.getYaw();
-  float pitch = filter.getPitch();
-  float roll = filter.getRoll();
-
-  float beta = filter.getBeta();
-
-
-  Serial.print("pitch: ");
-  Serial.print(pitch);
-  Serial.print(", yaw: ");
-  Serial.print(yaw);
-  Serial.print(", roll: ");
-  Serial.print(roll);
-  Serial.print(", beta: ");
-  Serial.print(beta);
-  Serial.println(" rad/s");
 }
