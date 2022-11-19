7a12c0c0844f0d2474ef88699ad599e22fb0624e
Semen (Sam) Hzhesiak <brutalsam@gmail.com>
2022-11-19 10:28:56 -0500

    refactoring, moved buttons stuff to a method

--- Esp32/BluetoothAirMouse/Airmouse.ino
+++ Esp32/BluetoothAirMouse/Airmouse.ino
@@ -1,4 +1,3 @@
-// Basic demo for accelerometer readings from Adafruit MPU6050
 #include "mpu_6050_setup.h"
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
@@ -7,13 +6,14 @@
 #include <Adafruit_AHRS.h>
 #include <math.h>
 #include <BleMouse.h>
+
 const float XDEG_MULTIPLIER = 28;
 const float YDEG_MULTIPLIER = 18;
 const float SENSITIVITY_THRESHOLD = 0.2;
-#define LEFT_BUTTON_PIN 13 // GIOP13 pin connected to button
-#define BACK_BUTTON_PIN 12 // GIOP13 pin connected to button
-BleMouse bleMouse;
+#define LEFT_BUTTON_PIN 13
+#define BACK_BUTTON_PIN 12
 
+BleMouse bleMouse;
 Adafruit_MPU6050 mpu;
 Adafruit_Madgwick filter;
 uint32_t timestamp;
@@ -21,14 +21,14 @@ Adafruit_Sensor_Calibration_EEPROM cal;
 float previousXValue;
 float previousYValue;
 
-// Variables will change:
-int leftLastState = HIGH; // the previous state from the input pin
-int leftCurrentState;     // the current reading from the input pin
+int leftLastState = HIGH;
+int leftCurrentState;
 
-int backLastState = HIGH; // the previous state from the input pin
-int backCurrentState;     // the current reading from the input pin
+int backLastState = HIGH;
+int backCurrentState;
 
-void setup(void) {
+void setup(void)
+{
   Serial.begin(115200);
   setup_mpu_6050(mpu);
 
@@ -37,16 +37,18 @@ void setup(void) {
   cal.loadCalibration();
   bleMouse.begin();
   bleMouse.setBatteryLevel(15);
-  Wire.setClock(400000); 
+  Wire.setClock(400000);
   pinMode(LEFT_BUTTON_PIN, INPUT_PULLUP);
   pinMode(BACK_BUTTON_PIN, INPUT_PULLUP);
   delay(100);
 }
 
-void loop() {
- 
-  if(bleMouse.isConnected()) {
-    if ((millis() - timestamp) < (15)) {
+void loop()
+{
+  if (bleMouse.isConnected())
+  {
+    if ((millis() - timestamp) < (15))
+    {
       return;
     }
     timestamp = millis();
@@ -62,7 +64,7 @@ void loop() {
     float gz = g.gyro.z * SENSORS_RADS_TO_DPS;
 
     filter.updateIMU(gx, gy, gz,
-              a.acceleration.x, a.acceleration.y, a.acceleration.z);
+                     a.acceleration.x, a.acceleration.y, a.acceleration.z);
 
     float yaw = filter.getYaw();
     float pitch = filter.getPitch();
@@ -71,48 +73,23 @@ void loop() {
     float beta = filter.getBeta();
 
     float xMoveValue = previousXValue - yaw;
-    if (previousXValue != 0 && abs(xMoveValue) <= SENSITIVITY_THRESHOLD) {
+    if (previousXValue != 0 && abs(xMoveValue) <= SENSITIVITY_THRESHOLD)
+    {
       xMoveValue = 0;
     }
 
     float yMoveValue = previousYValue - pitch;
-    if (previousYValue != 0 && abs(yMoveValue) <= SENSITIVITY_THRESHOLD) {
+    if (previousYValue != 0 && abs(yMoveValue) <= SENSITIVITY_THRESHOLD)
+    {
       yMoveValue = 0;
     }
 
     previousXValue = yaw;
     previousYValue = pitch;
 
-
-    // ToDO move buttons to a class/method
-    leftCurrentState = digitalRead(LEFT_BUTTON_PIN);
-    if (leftLastState == HIGH && leftCurrentState == LOW){
-      Serial.println("The button is pressed");
-      bleMouse.press(MOUSE_MIDDLE);
-      
-    }
-    else if (leftLastState == LOW && leftCurrentState == HIGH){
-      Serial.println("The button is released");
-      bleMouse.release(MOUSE_MIDDLE); 
-    }
-    // save the last state
-    leftLastState = leftCurrentState;
-
-    backCurrentState = digitalRead(BACK_BUTTON_PIN);
-    if (backLastState == HIGH && backCurrentState == LOW){
-      Serial.println("The button is pressed");
-      bleMouse.press(MOUSE_BACK);
-      
-    }
-    else if (backLastState == LOW && backCurrentState == HIGH){
-      Serial.println("The button is released");
-      bleMouse.release(MOUSE_BACK); 
-    }
-    // save the last state
-    backLastState = backCurrentState;
-
-    if (yMoveValue != 0 || xMoveValue !=0) {
-      // Move shit
+    processButtons(bleMouse);
+    if (yMoveValue != 0 || xMoveValue != 0)
+    {
       Serial.print("Mouse move: ");
       Serial.print(xMoveValue);
       Serial.print(", ");
@@ -120,4 +97,31 @@ void loop() {
       bleMouse.move(round(xMoveValue * XDEG_MULTIPLIER), -round(yMoveValue * YDEG_MULTIPLIER));
     }
   }
+}
+
+void processButtons(BleMouse bleMouse)
+{
+  leftCurrentState = digitalRead(LEFT_BUTTON_PIN);
+  if (leftLastState == HIGH && leftCurrentState == LOW)
+  {
+    bleMouse.press(MOUSE_MIDDLE);
+  }
+  else if (leftLastState == LOW && leftCurrentState == HIGH)
+  {
+    bleMouse.release(MOUSE_MIDDLE);
+  }
+  leftLastState = leftCurrentState;
+
+  backCurrentState = digitalRead(BACK_BUTTON_PIN);
+  if (backLastState == HIGH && backCurrentState == LOW)
+  {
+    Serial.println("The button is pressed");
+    bleMouse.press(MOUSE_BACK);
+  }
+  else if (backLastState == LOW && backCurrentState == HIGH)
+  {
+    Serial.println("The button is released");
+    bleMouse.release(MOUSE_BACK);
+  }
+  backLastState = backCurrentState;
 }
